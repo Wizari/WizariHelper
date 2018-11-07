60979a9a6e260b1c4b010c26abc834399804918f
Ivan Baranov <3903672@gmail.com>
2018-11-08 00:31:31 +0300

    Initial2

--- src/main/java/com/gmail/danze/framework/DanzeHelper.java
+++ src/main/java/com/gmail/danze/framework/DanzeHelper.java
@@ -3,17 +3,18 @@ package com.gmail.danze.framework;
 import java.util.Random;
 
 public class DanzeHelper {
-
+//Пререворот строки
     public static String reverseLine(String line) {
         String result = new StringBuffer(line).reverse().toString();
         return result;
     }
-
+//рандом возможно нужен import java.util.Random;
     public static int  generateRandom(int min, int max) {
         Random rnd = new Random();
         int number = min + rnd.nextInt(max - min + 1);
         return number;
     }
+//    -1+1
     public static char  thereAndHere(char downOne, char upOne) {
         char a = 'c' - 1;
         System.out.println(a);
